afee4dd2d5c7753c38ad0b6dbcc34ff50bab5a4d
Felipe de Almeida <felipeapellegrini@gmail.com>
2021-08-19 13:48:11 -0300

    test vercel

--- pages/index.js
+++ pages/index.js
@@ -6,22 +6,6 @@ function Home() {
             <h2>
                 Home
             </h2>
-            <Contador/>
-        </div>
-    )
-}
-
-function Contador() {
-    const [contador, setContador] = useState(1);
-
-    function addContador() {
-        setContador(contador+1);
-    }
-
-    return(
-        <div>
-            <div>{contador}</div>
-            <button onClick={addContador}>Adicionar</button>
         </div>
     )
 }
