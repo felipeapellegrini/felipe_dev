fed27bce95e3b980303d73c9d9e2b633b0503b96
Felipe de Almeida <felipeapellegrini@gmail.com>
2021-12-29 14:57:45 -0300

    create home page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,38 @@
-import { SignInButton } from '../SignInButton';
 import Image from 'next/image'
+import { useEffect, useState } from 'react';
 import styles from './styles.module.scss';
 
+interface ProfileProps {
+  avatar_url: string;
+}
+
 export function Header() {
+  const [profile, setProfile] = useState<ProfileProps>({} as ProfileProps);
+  useEffect(()=> {
+    fetch('https://api.github.com/users/felipeapellegrini')
+      .then(response => response.json())
+      .then(data => setProfile(data))
+  }, []);
+
+  console.log(profile);
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <Image
         src="/images/logo.svg"
         alt="header_img"
-        width={260}
+        width={150}
         height={50}/>
         <nav>
           <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <a>Portfolio</a>
+          <a>Repositórios</a>
+          <a>Contato</a>
         </nav>
-        <SignInButton />
+        <img
+          src={profile.avatar_url}
+          alt="github_profile"
+          className={styles.profile}/>
       </div>
     </header>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,13 @@
 import Head from 'next/head'
 
 export default function Home() {
+
+
   return (
     <>
       <Head>
         <title>Home | Felipe</title>
       </Head>
-      <h2>
-        Hello World
-      </h2>
     </>
   )
 }
