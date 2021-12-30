aac912e723f1795304b930d5f0f095a859c0d7fe
Felipe de Almeida <felipeapellegrini@gmail.com>
2021-12-30 09:05:58 -0300

    create repositorio and contato pages

---
+++ src/pages/contato/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+
+
+  return (
+    <>
+      <Head>
+        <title>Contato | Felipe de Almeida</title>
+      </Head>
+    </>
+  )
+}
---
+++ src/pages/repositorios/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+
+
+  return (
+    <>
+      <Head>
+        <title>Repositórios | Felipe de Almeida</title>
+      </Head>
+    </>
+  )
+}
